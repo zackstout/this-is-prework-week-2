13a6b565fe3a9100cbeb55939e4b2b1b3336a2c7
Millie Walsh <millie@primeacademy.io>
2017-03-31 16:03:59 -0500

    rename files to force order

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'assignment/*.js',
-      'tests/*.js'
+      'test/*.js'
     ],
 
 
