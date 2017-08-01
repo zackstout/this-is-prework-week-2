2f02cd3238b70597c57650e623e75430797415c5
Huck Brock <huckbrock@gmail.com>
2017-08-01 10:31:11 -0500

    added more argument calls to alerts and types

--- test/1_alerts.test.js
+++ test/1_alerts.test.js
@@ -1,12 +1,22 @@
 describe('basemode animalGuess()', function() {
-  it('triggers alert of "Try Again" when called with the argument panda', function() {
-
+  it('triggers alert of "Try Again" when called with an argument of "panda" or "elephant"', function() {
+    //Attempt incorrect guess
     animalGuess('panda');
+    
     expect(alertSpy.calledOnce).to.be.true;
 
     var alertArgs = alertSpy.args[0][0]; // grab alert args
     expect(alertArgs.toLowerCase()).to.contain('try again');
     expect(alertArgs.toLowerCase()).to.not.contain('How did you know');
+
+    //Another incorrect guess
+    animalGuess('elephant');
+    
+    expect(alertSpy.calledTwice).to.be.true;
+
+    var alertArgs2 = alertSpy.args[1][0]; // grab alert args    
+    expect(alertArgs2.toLowerCase()).to.contain('try again');
+    expect(alertArgs2.toLowerCase()).to.not.contain('How did you know');
   });
 
   it('triggers alert of "How did you know?!" when called with the argument turtle', function() {
--- test/2_types.test.js
+++ test/2_types.test.js
@@ -1,7 +1,7 @@
 describe('basemode theDoubler()', function() {
   it('logs the correct responses based on type', function() {
 
-    var testArray = [4,"Hey", true, {}, undefined, false];
+    var testArray = [4, 11, "Hey", "frog", true, {}, undefined, false];
 
     testArray.forEach(function(thing){
         theDoubler(thing);
@@ -9,10 +9,12 @@ describe('basemode theDoubler()', function() {
 
     var logArgs = logSpy.args; // grab alert args
     expect(logArgs[0][0]).to.equal(8);
-    expect(logArgs[1][0]).to.equal('HeyHey');
-    expect(logArgs[2][0].toLowerCase()).to.contain('super-duper true');
-    expect(logArgs[3][0].toLowerCase()).to.contain('everything is an object');
-    expect(logArgs[4][0].toLowerCase()).to.contain("i can't double this");
-    expect(logArgs[5][0].toLowerCase()).to.contain("i won't double this");
+    expect(logArgs[1][0]).to.equal(22);
+    expect(logArgs[2][0]).to.equal('HeyHey');
+    expect(logArgs[3][0]).to.equal('frogfrog');
+    expect(logArgs[4][0].toLowerCase()).to.contain('super-duper true');
+    expect(logArgs[5][0].toLowerCase()).to.contain('everything is an object');
+    expect(logArgs[6][0].toLowerCase()).to.contain("i can't double this");
+    expect(logArgs[7][0].toLowerCase()).to.contain("i won't double this");
   });
 });
